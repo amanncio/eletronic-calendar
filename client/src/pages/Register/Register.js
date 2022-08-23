f5b71d2e8251a62cd0f098e8118897c0b59adf08
Matheus Amâncio <matheus.amancio01@gmail.com>
2022-08-23 16:27:01 -0300

    [Conexão do Register com o Banco de Dados-p2 + Icone favicon]

--- client/src/pages/Register/Register.js
+++ client/src/pages/Register/Register.js
@@ -1,14 +1,39 @@
-import React from 'react'
+import React, {useState} from 'react';
 import "./Register.css"
 
+
+import Axios from "axios";
 const Register = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const register = () => {
+    Axios.post("http://localhost:3001/user/cadastro", {
+      username: username,
+      password: password,
+    }).then((response) => {
+      console.log(response);
+    });
+  };
+
   return (
     <div className='register'>
       <h1>Faça seu <span>cadastro</span></h1>
       <div className='register-form'>
-          <input type="text" placeholder="Nome de usuário" />
-          <input type="password" placeholder="Digite sua senha" />
-          <button>Cadastrar</button>
+          <input
+            type="text"
+            placeholder="Nome de usuário"
+            onChange={(event) => {
+              setUsername(event.target.value);
+            }}
+           />
+          <input type="password"
+            placeholder="Digite sua senha"
+            onChange={(event) => {
+              setPassword(event.target.value);
+            }}  
+          />
+          <button onClick={register}>Cadastrar</button>
       </div>
     </div>
   )
