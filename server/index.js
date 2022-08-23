f16c1172717ea178effbd726d430646815f81683
Matheus Amâncio <matheus.amancio01@gmail.com>
2022-08-23 15:01:03 -0300

    [Teste da conexão com o banco de dados OK]

--- server/index.js
+++ server/index.js
@@ -1,6 +1,24 @@
 const express = require("express");
 const app =  express();
 
+const mysql = require('mysql');
+const db = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'amancio',
+  password : 'amancio',
+  database : 'eletronic_calendar'
+});
+
+
+app.get("/cadastro", (req, res) => {
+    db.query(
+        "INSERT INTO users (username, password) VALUES ('Amancio', 'testandosenha');",
+        (err,  results) => {
+            console.log(err)
+            res.send(results);
+        }
+    )
+})
 app.listen(3001, (req, res) => {
     console.log("Its Running...");
 })
